ab0d00e36888cb793baca313e45a8ac726bae411
chlyun <rkdmf1306@gmail.com>
2024-06-08 18:32:37 +0900

    1차 과제 mission 0

---
+++ mission0/mission.js
@@ -0,0 +1,32 @@
+// 문제1
+function getValueAtObject(obj, key) {
+  return obj[key] !== undefined ? obj[key] : Error("Error!");
+}
+
+const person = {
+  name: "Alice",
+  age: 25,
+  city: "Wonderland",
+};
+
+console.log(getValueAtObject(person, "name")); // 'Alice'
+console.log(getValueAtObject(person, "age")); // 25
+console.log(getValueAtObject(person, "city")); // 'Wonderland'
+console.log(getValueAtObject(person, "country")); // Error !
+
+// 문제2
+
+function getNumberAtArray(arr, index) {
+  if (index >= 0 && arr.length > index) {
+    return arr[index];
+  } else {
+    return Error("Error!");
+  }
+}
+
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(getNumberAtArray(numbers, 2)); // 30
+console.log(getNumberAtArray(numbers, 4)); // 50
+console.log(getNumberAtArray(numbers, 5)); // Error!
+console.log(getNumberAtArray(numbers, -1)); // Error!
